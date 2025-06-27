266ed35efcf482c0b1555a1c7a734d4b51207ca9
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-06-27 08:13:59 -0300

    Adapta cabeçalho para desktop

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
       <div className="max-width">
         <div
           id="header-content"
-          className="flex justify-between items-center py-12"
+          className="flex justify-between items-center py-12 md:flex-row-reverse"
         >
           <button type="button" className="p-3 bg-secondary rounded-lg">
             <Moon size={32} />
@@ -17,7 +17,7 @@ export const Header = () => {
             <img src={Logo} alt="Logo" className="w-full max-w-32" />
           </a>
 
-          <button type="button">
+          <button type="button" className="md:hidden">
             <Menu size={48} />
           </button>
         </div>
