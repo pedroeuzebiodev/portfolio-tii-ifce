45f1e82b2d264ab89fc16ddf5b321d48abdf461c
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-06-27 16:29:45 -0300

    Adapta seção de início para desktop

--- src/components/section-home.jsx
+++ src/components/section-home.jsx
@@ -6,14 +6,14 @@ export const SectionHome = () => {
       <div className="max-width">
         <div
           id="section-home-content"
-          className="py-12 flex flex-col gap-12 items-center justify-center"
+          className="py-12 flex flex-col gap-12 items-center justify-center md:flex-row md:items-start md:gap-24"
         >
-          <div id="section-home-content-texts" className="space-y-8">
-            <h1 className="text-primary text-3xl font-bold">
+          <div id="section-home-content-texts" className="space-y-[1.875rem]">
+            <h1 className="text-primary text-3xl font-bold md:text-4xl">
               Técnico em Informática <br /> para Internet
             </h1>
 
-            <p className="text-xl">
+            <p className="text-xl md:text-2xl">
               Prepara o aluno para desenvolver softwares, aplicativos e soluções
               web, além de atuar em áreas como desenvolvimento de sites,
               comércio eletrônico e marketing digital.
@@ -21,13 +21,17 @@ export const SectionHome = () => {
 
             <a
               href="#"
-              className="px-6 py-3 bg-secondary rounded-lg text-lg font-medium"
+              className="px-6 py-3 bg-secondary rounded-lg text-lg font-medium md:text-xl"
             >
               Saiba mais
             </a>
           </div>
 
-          <img src={Notebook} alt="Notebook" className="w-full rounded-lg" />
+          <img
+            src={Notebook}
+            alt="Notebook"
+            className="w-full max-w-md rounded-lg"
+          />
         </div>
       </div>
     </section>
