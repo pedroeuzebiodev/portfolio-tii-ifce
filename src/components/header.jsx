09aac98e3d32465e290cf9a171b216fb7a3e4a1a
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-07-01 19:55:59 -0300

    Atualiza header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
       <div className="max-width">
         <div
           id="header-content"
-          className="flex justify-between items-center md:flex-row-reverse"
+          className="flex justify-between items-center lg:flex-row-reverse"
         >
           <button type="button" className="p-3 bg-secondary rounded-lg">
             <Moon size={24} />
@@ -17,7 +17,7 @@ export const Header = () => {
             <img src={Logo} alt="Logo" className="w-full max-w-32" />
           </a>
 
-          <button type="button" className="md:hidden">
+          <button type="button" className="lg:hidden">
             <Menu size={48} />
           </button>
         </div>
