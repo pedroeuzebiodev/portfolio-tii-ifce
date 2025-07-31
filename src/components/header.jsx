e9b7d0fcb298d52ee324b8f66f904ebd30dc3e04
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-07-30 21:02:02 -0300

    Remove toggle

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,11 @@
 import Logo from "../assets/logo.png";
-import { Moon, Menu } from "lucide-react";
+import { Menu } from "lucide-react";
 
 export const Header = () => {
   return (
     <header className="py-12">
       <div className="max-width">
-        <div
-          id="header-content"
-          className="flex justify-between items-center lg:flex-row-reverse"
-        >
-          <button type="button" className="p-3 bg-secondary rounded-lg">
-            <Moon size={24} />
-          </button>
-
+        <div id="header-content" className="flex justify-between items-center">
           <a href="/">
             <img src={Logo} alt="Logo" className="w-full max-w-32" />
           </a>
