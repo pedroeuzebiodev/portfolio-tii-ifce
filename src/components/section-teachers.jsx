0c0dd85ec20bf2baeb2e19feed0375410ad7e889
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-07-30 22:30:49 -0300

    Ajusta a seção dos professores para incluir altura fixa nos cards

--- src/components/section-teachers.jsx
+++ src/components/section-teachers.jsx
@@ -9,7 +9,7 @@ import LogoYoutube from "../assets/logo-youtube.svg";
 
 export const SectionTeachers = () => {
   return (
-    <section id="section-teachers" className="py-12">
+    <section id="section-teachers">
       <div className="max-width">
         <div id="section-teachers-content" className="space-y-[1.875rem]">
           <h2 className="text-primary text-2xl lg:text-3xl font-semibold text-center">
@@ -23,9 +23,9 @@ export const SectionTeachers = () => {
 
           <div
             id="teachers"
-            className="flex justify-center items-center gap-[1.875rem] flex-wrap lg:flex-nowrap p-[1.875rem]"
+            className="flex justify-center items-center gap-[1.875rem] flex-wrap lg:flex-nowrap"
           >
-            <div className="teacher flex flex-col justify-center items-center gap-4 bg-secondary rounded-lg p-[1.875rem] w-full max-w-96">
+            <div className="teacher flex flex-col justify-center items-center gap-4 bg-secondary rounded-lg p-[1.875rem] w-full max-w-96 h-96">
               <img src={Cintia} alt="Cintia" className="w-full max-w-32" />
 
               <h3 className="text-xl lg:text-2xl font-semibold text-primary text-center">
@@ -64,7 +64,7 @@ export const SectionTeachers = () => {
               </div>
             </div>
 
-            <div className="teacher flex flex-col justify-center items-center gap-4 bg-secondary rounded-lg p-[1.875rem] w-full max-w-96">
+            <div className="teacher flex flex-col justify-center items-center gap-4 bg-secondary rounded-lg p-[1.875rem] w-full max-w-96 h-96">
               <img
                 src={Jefferson}
                 alt="Jefferson"
@@ -101,7 +101,7 @@ export const SectionTeachers = () => {
               </div>
             </div>
 
-            <div className="teacher flex flex-col justify-center items-center gap-4 bg-secondary rounded-lg p-[1.875rem] w-full max-w-96">
+            <div className="teacher flex flex-col justify-center items-center gap-4 bg-secondary rounded-lg p-[1.875rem] w-full max-w-96 h-96">
               <img src={Kelvia} alt="Kelvia" className="w-full max-w-32" />
 
               <h3 className="text-xl lg:text-2xl font-semibold text-primary text-center">
